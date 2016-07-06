110c05900faa8326fe26cc3a2821fbc7a94638f3
takumi ishida <ishida06takumi04@gmail.com>
2016-07-06 10:38:45 +0900

    first commit

--- src/app.js
+++ src/app.js
@@ -16,13 +16,11 @@ var GameLayer = cc.Layer.extend({
         label.setPosition(size.width * 3 / 6, size.height * 4 / 5);
         this.addChild(label, 4);
 
-        for(var i = 0 ;i < 4 ;i++){
-          for(var j = 0;j < 4;j++){
-            var cover = cc.Sprite.create(res.cover_png);
-            cover.setPosition(size.width - (j*70+55),size.height- (i*70+150));
-            this.addChild(cover)
-          }
+        for(i=0;i<16;i++){
+          var tile = cc.Sprite.create(res.cover_png);
+          this.addChild(tile,0);
+          //タイルを格子状に配置する計算式
+          tile.setPosition(49+i%4*74,320-Math.floor(i/4)*74);
         }
-
       },
 });
