115c29c8c1a71b56350a053c7b68414e02bd62fa
takumi ishida <ishida06takumi04@gmail.com>
2016-07-08 11:44:09 +0900

    神経衰弱ゲームデザイン変更

--- src/app.js
+++ src/app.js
@@ -1,64 +1,99 @@
-var GameScene = cc.Scene.extend({
-    onEnter: function() {
-        this._super();
-        var backgroundLayer = new cc.LayerColor(new cc.Color(140, 200, 140, 128));
+var gameArray = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15,16,16,17,17];
+var pickedTiles = [];
+var scoreText;
+var gameoverText;
+var miss=0;
+var gameover=0;
+var console_label;
+
+var shuffle = function(v){
+    for(var j, x, i = v.length; i; j = parseInt(Math.random() * i),x = v[--i], v[i] = v[j], v[j] = x);
+        return v;
+};
+
+var gameScene = cc.Scene.extend({
+    onEnter:function () {
+
+      var backgroundLayer = new cc.LayerColor(new cc.Color(140, 200, 140, 128));
         this.addChild(backgroundLayer);
-        var gamelayer = new GameLayer();
-        this.addChild(gamelayer);
+
+        gameArray = shuffle(gameArray);
+        this._super();
+        gameLayer = new game();
+        gameLayer.init();
+        this.addChild(gameLayer);
     }
 });
 
-var console_label;
-
-var GameLayer = cc.Layer.extend({
-    ctor: function() {
+var game = cc.Layer.extend({
+    init:function () {
         this._super();
-
-        var size = cc.director.getWinSize();
-        var gradient = cc.LayerGradient.create(cc.color(50,0,50,255),cc.color(0xFF,0x00,0xFF,255));
+        var gradient = cc.LayerGradient.create(cc.color(50,0,50,255), cc.color(0xFF,0x00,0xFF,255));
         this.addChild(gradient);
-        for(i=0;i<16;i++){
-          //var tile = cc.Sprite.create(res.cover_png);
-          var tile = new MemoryTile();
-          this.addChild(tile,0);
-          //タイルを格子状に配置する計算式
-          tile.setPosition(49+i%4*74,320-Math.floor(i/4)*74);
+        scoreText = cc.LabelTTF.create("Miss: 0","Arial","32",cc.TEXT_ALIGNMENT_CENTER);
+        this.addChild(scoreText);
+        scoreText.setPosition(90,50);
+        gameoverText = cc.LabelTTF.create("GameOver: "+gameover,"Arial","32",cc.TEXT_ALIGNMENT_CENTER);
+        this.addChild(gameoverText);
+        gameoverText.setPosition(350,50);
+        for(i=0;i<36;i++){
+            var tile = new MemoryTile();
+            tile.pictureValue = gameArray[i];
+            this.addChild(tile,0);
+            //タイルを格子状に配置する計算式
+            tile.setPosition(49+i%6*76,480-Math.floor(i/6)*74);
         }
-        console_label = cc.LabelTTF.create("神経衰弱ゲーム","Arial",26);
-        console_label.setPosition(size.width * 3 / 6, size.height * 4 / 5);
-        this.addChild(console_label,1)
-        return true;
-      },
+    }
 });
-//Spriteクラスを拡張して実装してみた
 //Spriteクラスを拡張して実装してみた
 var MemoryTile = cc.Sprite.extend({
     ctor:function() {
         this._super();
         this.initWithFile(res.cover_png);
-        //イベントマネージャにイベントリスナーを追加
-        cc.eventManager.addListener(listener.clone(),this);
-    },
+        cc.eventManager.addListener(listener.clone(), this);
+    }
 });
-
 //listnerの宣言
 var listener = cc.EventListener.create({
     event: cc.EventListener.TOUCH_ONE_BY_ONE,
     swallowTouches: true,
     onTouchBegan: function (touch, event) {
-      var target = event.getCurrentTarget();
-      var location = target.convertToNodeSpace(touch.getLocation());
-      var targetSize = target.getContentSize();
-      var targetRectangle = cc.rect(0, 0, targetSize.width, targetSize.height);
-
-      if (cc.rectContainsPoint(targetRectangle, location)) {
-          console.log("I piced a tile!!");
-          console_label.setString("I piced a tile!!");
-          // 一秒後に消える
-       setTimeout(function() {
-           console_label.setString("");
-       }, 1500);
-
-      }
+        if(pickedTiles.length<2){
+            var target = event.getCurrentTarget();
+            var location = target.convertToNodeSpace(touch.getLocation());
+            var targetSize = target.getContentSize();
+            var targetRectangle = cc.rect(0, 0, targetSize.width, targetSize.height);
+            if (cc.rectContainsPoint(targetRectangle, location)) {
+                if(pickedTiles[0] != target){
+                  target.initWithFile("res/tile_"+target.pictureValue+".png");
+                  pickedTiles.push(target);
+                  if(pickedTiles.length==2){
+                      checkTiles();
+                  }
+                }
+            }
+        }
     }
 });
+
+function checkTiles(){
+    var pause = setTimeout(function(){
+        if(pickedTiles[0].pictureValue!=pickedTiles[1].pictureValue){
+            pickedTiles[0].initWithFile(res.cover_png);
+            pickedTiles[1].initWithFile(res.cover_png);
+            miss++;
+            scoreText.setString("Miss: "+miss);
+            if(miss == 3){
+              miss = 0;
+              gameover++;
+              cc.director.runScene(new GameoverScene());
+            }
+
+        }
+        else{
+            gameLayer.removeChild(pickedTiles[0]);
+            gameLayer.removeChild(pickedTiles[1]);
+        }
+        pickedTiles = [];
+    },500);
+}
--- src/resource.js
+++ src/resource.js
@@ -1,6 +1,5 @@
 //resource.js
 var res = {
-    HelloWorld_png : "res/HelloWorld.png",
     cover_png : "res/cover.png",
     tile0_png : "res/tile_0.png",
     tile1_png : "res/tile_1.png",
@@ -10,6 +9,16 @@ var res = {
     tile5_png : "res/tile_5.png",
     tile6_png : "res/tile_6.png",
     tile7_png : "res/tile_7.png",
+    tile8_png : "res/tile_8.png",
+    tile9_png : "res/tile_9.png",
+    tile10_png : "res/tile_10.png",
+    tile11_png : "res/tile_11.png",
+    tile12_png : "res/tile_12.png",
+    tile13_png : "res/tile_13.png",
+    tile14_png : "res/tile_14.png",
+    tile15_png : "res/tile_15.png",
+    tile16_png : "res/tile_16.png",
+    tile17_png : "res/tile_17.png",
 
 };
 
